0f1f96414a9ae164b84f169c47cae17b7e3bf1b9
heysaty <satyam@gkmit.co>
2022-11-07 10:48:20 +0530

    leave api call from frontend

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import models
 from database import engine
 from routes import login, signup, logout, leaves
 
-models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=engine)
 
 app.include_router(login.router)
 app.include_router(signup.router)
